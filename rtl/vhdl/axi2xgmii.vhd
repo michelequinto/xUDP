d0ce125ca48d617187d34337329d8f554d94dc5f
Michele <michele.quinto@cern.ch>
2016-10-18 19:34:33 +0200

    connection axi to and from xgmii

--- rtl/vhdl/axi2xgmii.vhd
+++ rtl/vhdl/axi2xgmii.vhd
@@ -37,7 +37,7 @@ use work.xUDP_Common_pkg.all;
 
 entity axi2xgmii is
     generic (
-        CHANGE_EDIANESS : natural := 1
+        CHANGE_EDIANESS : boolean := true
       );
     port (
       aclk                      : in std_logic;
@@ -59,28 +59,51 @@ architecture rtl of axi2xgmii is
 
  begin
 
-  pkt_tx_data <= axi4.tdata;
-  pkt_tx_val <= axi4.tvalid;
-  pkt_tx_eop <= axi4.tlast;
-  
-  generate_sop: process(aclk)
-    variable tvalid_d : std_logic := '0';
-  begin
-    if rising_edge(aclk) then
-      pkt_tx_sop <= axi4.tvalid and (not tvalid_d);
-      tvalid_d := axi4.tvalid;
-    end if;
-  end process;        
+   do_not_change_endianess : if not CHANGE_EDIANESS generate
+   
+     pkt_tx_data <= axi4.tdata;
+     
 
-  tready <= not pkt_tx_full;  
-    
-  with axi4.tkeep select pkt_tx_mod  <= "000" when "11111111",
-                                        "001" when "00000001",             
-                                        "010" when "00000011",            
-                                        "011" when "00000111",      
-                                        "100" when "00001111",
-                                        "101" when "00011111",
-                                        "110" when "00111111",
-                                        "111" when "01111111",
-                                        "XXX" when others;
+     with axi4.tkeep select pkt_tx_mod  <= "000" when "11111111",
+                                           "001" when "00000001",             
+                                           "010" when "00000011",            
+                                           "011" when "00000111",      
+                                           "100" when "00001111",
+                                           "101" when "00011111",
+                                           "110" when "00111111",
+                                           "111" when "01111111",
+                                           "XXX" when others;
+   end generate;
+
+   change_endianess : if CHANGE_EDIANESS generate
+   
+     bytes_mingling : for i in 0 to axi4.tdata'length/8-1 generate
+       pkt_tx_data(8*i-1 downto i*8) <= axi4.tdata(axi4.tdata'length-1-8*i downto axi4.tdata'length-8*(i+1));
+     end generate;
+     
+     with axi4.tkeep select pkt_tx_mod  <= "000" when "11111111",
+                                           "001" when "10000000",             
+                                           "010" when "11000000",            
+                                           "011" when "11100000",      
+                                           "100" when "11110000",
+                                           "101" when "11111000",
+                                           "110" when "11111100",
+                                           "111" when "11111110",
+                                           "XXX" when others;
+   end generate;
+     
+   generate_sop: process(aclk)
+     variable tvalid_d : std_logic := '0';
+   begin
+     if rising_edge(aclk) then
+       pkt_tx_sop <= axi4.tvalid and (not tvalid_d);
+       tvalid_d := axi4.tvalid;
+     end if;
+   end process;        
+
+   tready <= not pkt_tx_full;
+   
+   pkt_tx_val <= axi4.tvalid;
+   pkt_tx_eop <= axi4.tlast; 
+   
 end rtl;
